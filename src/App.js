8b5cfaac72be89243cac38ba7d659806e7fb788c
mightyFZeus <67365597+mightyFZeus@users.noreply.github.com>
2021-08-19 00:23:38 +0100

    logo

--- src/App.js
+++ src/App.js
@@ -3,20 +3,24 @@ import {Grid, LinearProgress, Card, CardContent, CardMedia} from '@material-ui/c
 import me from './me2.png'
 import useStyles from './Styles'
 import DisplayName from './DisplayName'
+import zuri from './zuri.png'
+import hng from './hng.png'
 
 
 export default function App() {
   const classes = useStyles()
   return (
     <div className="App">
+      
     <DisplayName />
+    <div className={classes.toolbar} />
     <Grid container spacing={3}>
     <Grid item sm={12} md={4}>
       <Card>
         <CardContent>
          <img src={me} className='test' alt='profile-pic' />
-        <p>BOLARIN OLABISI</p>
-        <p>Frontend Dev</p>
+        <p style={{fontWeight:'bold', textAlign:'center'}}>BOLARIN OLABISI</p>
+        <p style={{fontWeight:'bold', textAlign:'center'}}>Frontend Dev</p>
         </CardContent>
       </Card>
     </Grid>
@@ -33,16 +37,16 @@ export default function App() {
         <div className='personal'>
           <div>
             
-            <p>Adress: 123 lorem </p>
-            <p>Email: bolarinolabisi36@gmail.com </p>
-            <p>phone: 09015752977</p>
+            <p><span style={{fontWeight:'bold'}}>Adress</span>: Ibadan Oyo State, Nigeria </p>
+            <p><span style={{fontWeight:'bold'}}>Email</span>: bolarinolabisi36@gmail.com </p>
+            <p><span style={{fontWeight:'bold'}}>Phone</span>: 09015752977</p>
           </div>
           <div>
-            <p>main Language: Yoruba</p>
-            <p>Second Language: English</p>
-            <p>Native Language: Yoruba</p>
-            <p>Native Language: Yoruba</p>
-            <p>Nationality: Nigerian</p>
+            <p><span style={{fontWeight:'bold'}}>Main Language</span>: Yoruba</p>
+            <p><span style={{fontWeight:'bold'}}>Second Language</span>: English</p>
+            <p><span style={{fontWeight:'bold'}}>Native Language</span>: Yoruba</p>
+           
+            <p><span style={{fontWeight:'bold'}}>Nationality</span>: Nigerian</p>
           </div>
         </div>
         </CardContent>
@@ -59,28 +63,28 @@ export default function App() {
             <div className='prof'>
               <div>
               
-              <p>
+              <p style={{fontWeight:'bold'}}>
                 HTML
               <LinearProgress variant="determinate" value='90'  />
               </p>
-              <p>
+              <p style={{fontWeight:'bold'}}>
                CSS
               <LinearProgress variant="determinate" value='90'  />
               </p>
-              <p>
+              <p style={{fontWeight:'bold'}}>
                JAVASCRIPT
               <LinearProgress variant="determinate" value='90'  />
               </p>
               
-              <p>
+              <p style={{fontWeight:'bold'}}>
                 REACT JS
               <LinearProgress variant="determinate" value='90'  />
               </p>
-              <p>
+              <p style={{fontWeight:'bold'}}>
                REACT NATIVE
               <LinearProgress variant="determinate" value='90'  />
               </p>
-              <p>
+              <p style={{fontWeight:'bold'}}>
                TYPESCRIPT
               <LinearProgress variant="determinate" value='90'  />
               </p>
@@ -99,12 +103,12 @@ export default function App() {
             <h4>Education</h4>
         <div className='education-div'>
           <div>
-            <p>University</p>
+            <p style={{textDecoration:'underline'}}>University</p>
             <p>University Of Abuja</p>
             <p>Bsc Computer Science</p>
           </div>
           <div>
-            <p>SSCE</p>
+            <p style={{textDecoration:'underline'}}>SSCE</p>
             <p>Oyun Baptist high school,ijagbo</p>
             <p>Bsc Computer Science</p>
           </div>
@@ -123,7 +127,7 @@ export default function App() {
               <CardContent>
               <div>
             <div>
-              <p>Intership @</p>
+              <p style={{textDecoration:'underline'}}>Intership @</p>
               <p>1. ITMS(University of Abuja, Abuja), 2016-2018</p>
               <p>2. HNGi8, 2021</p>
             </div>
@@ -135,13 +139,22 @@ export default function App() {
             </Card>
         </Grid>
       </Grid>
-      <Grid>
-        <div>
-        <img />
-        <img />
-        <img />
+    <Grid container justifyContent='flex-end'>
+    <Grid item sm={12} md={8}>
+        <div className={classes.toolbar} />
+        <div className='image'>
+          <a href='https://internship.zuri.team/'>
+                 <img src={zuri} style={{width:'200px'}} />
+          </a>
+          <a href="https://hng.tech/">
+             <img src={hng} style={{width:'50px'}} />
+        
+          </a>
+   
+       
         </div>
       </Grid>
+    </Grid>
     </div>
   );
 }
--- src/DisplayName.js
+++ src/DisplayName.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import {Button} from '@material-ui/core'
 const DisplayName = () => {
   const [showName, setShowName] = useState(false);
 
@@ -8,8 +8,8 @@ const DisplayName = () => {
   };
   return (
     <>
-      {showName && <p>BOLARIN OLABISI</p>}
-      <button onClick={showMyname}>show My Name</button>
+      {showName && <p style={{textAlign:'center'}} >BOLARIN OLABISI</p>}
+      <Button style={{backgroundColor:'#6c63fe', color:'white', textDecoration:'none', display: showName? 'none' : ''}} onClick={showMyname}>show My Name</Button >
     </>
   );
 };
